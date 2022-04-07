93a0fc7974ea024f8510559de63cda3d0dafece0
Pratyush Kumar <pratyush.019@gmail.com>
2022-04-07 14:19:37 -0500

    exported weighted_stat_diff

--- dswe/__init__.py
+++ dswe/__init__.py
@@ -3,4 +3,4 @@ from .tempGP import TempGP
 from .funGP import FunGP
 from .covmatch import CovMatch
 from .comparePCurve import ComparePCurve
-from ._comparePCurve_subroutine import generate_test_set, compute_weighted_diff
+from ._comparePCurve_subroutine import generate_test_set, compute_weighted_diff, compute_weighted_stat_diff
