a8e4bc787adc35f3d6a49c6ad138d0622b3f231a
Pratyush Kumar <pratyush.019@gmail.com>
2022-04-13 11:51:54 -0500

    added reshape when features has one columns

--- dswe/AMK.py
+++ dswe/AMK.py
@@ -18,9 +18,10 @@ class AMK(object):
             raise ValueError(
                 "The X_test should be either a list or numpy array or dataframe.")
 
-        if X_train.shape[1] != X_test.shape[1]:
-            raise ValueError(
-                "The number of features in train and test set must be same.")
+        if len(X_train.shape) > 1:
+            if X_train.shape[1] != X_test.shape[1]:
+                raise ValueError(
+                    "The number of features in train and test set must be same.")
 
         if not (isinstance(y_train, list) or isinstance(y_train, np.ndarray)):
             raise ValueError(
@@ -30,13 +31,16 @@ class AMK(object):
             raise ValueError(
                 "The X_train and y_train should have same number of data points.")
 
-        n_cov = X_train.shape[1]
+        if len(X_train.shape) > 1:
+            ncov = X_train.shape[1]
+        else:
+            ncov = 1
 
         if not (isinstance(bw, list) or isinstance(bw, np.ndarray)):
             if bw not in ['dpi', 'dpi_gap']:
                 raise ValueError(
                     "The bw must a list or an array or set to 'dpi' or 'dpi_gap'.")
-        elif len(bw) != n_cov:
+        elif len(bw) != ncov:
             raise ValueError(
                 "The length of bw must be same as the number of covariates.")
 
@@ -45,25 +49,25 @@ class AMK(object):
                 raise ValueError(
                     "The n_multi_cov must be set to 'all' or 'none' or an integer.")
 
-        if n_cov == 1:
+        if ncov == 1:
             n_multi_cov = 'all'
-        elif n_cov == 2:
+        elif ncov == 2:
             if n_multi_cov != 'none':
                 n_multi_cov = 'all'
 
         if n_multi_cov not in ['all', 'none']:
-            if n_multi_cov < 1 and n_multi_cov > n_cov:
+            if n_multi_cov < 1 and n_multi_cov > ncov:
                 raise ValueError(
                     "if n_multi_cov is not set to 'all' or 'none', then it must be set to an integer greater than 1, and less than or equal to the number of covariates.")
-            elif n_multi_cov == n_cov:
+            elif n_multi_cov == ncov:
                 n_multi_cov = 'all'
                 fixed_cov = None
-            elif n_multi_cov < n_cov:
+            elif n_multi_cov < ncov:
                 if fixed_cov is not None:
                     if not (isinstance(fixed_cov, list) or isinstance(fixed_cov, np.ndarray)):
                         raise ValueError(
                             "The fixed_cov should either be a list or an array or set to None.")
-                    elif len(list(set(fixed_cov).intersection(list(range(n_cov))))) != len(fixed_cov):
+                    elif len(list(set(fixed_cov).intersection(list(range(ncov))))) != len(fixed_cov):
                         raise ValueError(
                             "Any or all the values in fixed_cov exceeds the number of columns in X_train.")
                     elif len(fixed_cov) >= n_multi_cov:
@@ -76,16 +80,20 @@ class AMK(object):
             if not (isinstance(cir_cov, list) or isinstance(cir_cov, np.ndarray)):
                 raise ValueError(
                     "The cir_cov should be a list or an array or set to None.")
-            elif len(list(set(cir_cov).intersection(list(range(n_cov))))) != len(cir_cov):
+            elif len(list(set(cir_cov).intersection(list(range(ncov))))) != len(cir_cov):
                 raise ValueError(
                     "Any or all the values in cir_cov exceeds the number of columns in X_train.")
 
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
+        self.X_train = np.array(X_train)
+        self.y_train = np.array(y_train)
+        self.X_test = np.array(X_test)
         self.bw = bw
         self.n_multi_cov = n_multi_cov
         self.fixed_cov = fixed_cov
         self.cir_cov = cir_cov
 
-        return self
+        if len(self.X_train.shape) == 1:
+            self.X_train = self.X_train.reshape(-1, 1)
+            self.X_test = self.X_test.reshape(-1, 1)
+
+        return None
