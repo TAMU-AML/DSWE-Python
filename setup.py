d6e0e81ab4005b9f120e54aa9d87ed7dd6ef50f7
Pratyush Kumar <pratyush.019@gmail.com>
2022-07-06 10:54:09 -0500

    simplify setup.py, included requirements.txt in manifest.in

--- setup.py
+++ setup.py
@@ -7,24 +7,25 @@ with open("requirements.txt", "r") as f:
     install_requires = f.read().splitlines()
 
 setup(
-    name="DSWE",
-    version="0.0.1",
+    name="dswe",
+    version="0.0.2",
     author="Pratyush Kumar, Abhinav Prakash, and Yu Ding",
     author_email="yuding@tamu.edu",
-    maintainer_email="pratyush19@tamu.edu",
+    maintainer_email="pratyush.019@gmail.com",
     description="A python package to supplement the Data Science for Wind Energy (DSWE) book.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TAMU-AML/DSWE-Python",
+    url="https://tamu-aml.github.io/DSWE-Python/",
     project_urls={
+        "Source": "https://github.com/TAMU-AML/DSWE-Python",
         "Bug Tracker": "https://github.com/TAMU-AML/DSWE-Python/issues",
     },
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=install_requires,
-    packages=find_packages(exclude=["doc"]),
+    packages=find_packages(exclude=["docs"]),
     python_requires=">=3.6",
 )
