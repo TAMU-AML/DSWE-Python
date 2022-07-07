4e1e3a32bdf83f43b8a2c108ed83a38b3851159d
Pratyush Kumar <pratyush.019@gmail.com>
2022-07-07 11:55:16 -0500

    updated docs for version=0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Pratyush Kumar, Abhinav Prakash, and Yu Ding'
 author = 'Pratyush Kumar, Abhinav Prakash, and Yu Ding'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
